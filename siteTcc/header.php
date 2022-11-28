15a0912dd502a389025c3a7015a6ca1c0db26616
Estephany000 <estephanys000@gmail.com>
2022-11-28 03:08:52 +0000

    Deu certo!

--- siteTcc/header.php
+++ siteTcc/header.php
@@ -1,10 +1,10 @@
    <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-light shadow">
+    <nav class="navbar navbar-expand-lg navbar-dark shadow  ">
         <div class="container d-flex justify-content-between align-items-center">
 
-            <a class="navbar-brand text-success logo h1 align-self-center" href="index.html">
-                Zay
-            </a>
+            <a href="index.php" style="text-decoration-line: none;"><span class="navbar-brand text-success logo h1 align-self-center">
+                Body<span class="navbar-brand text-dark logo h1 align-self-center" href="index.php">Fitness</span>
+            </span></a>
 
             <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -12,18 +12,18 @@
 
             <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
                 <div class="flex-fill">
-                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                        <li class="nav-item">
-                            <a class="nav-link"  id="home" href="">Home</a>
+                    <ul class="nav nav-tabs nav-stacked navbar-nav d-flex justify-content-between mx-lg-auto">
+                        <li class="nav-item ">
+                            <a class="nav-link text-dark"  id="home" href="">Home</a></li>
                         </li>
                         <li class="nav-item" >
-                            <a class="nav-link"  id="catalogo" href="">Catálogo</a>
+                            <a class="nav-link text-dark"  id="catalogo" href="">Catálogo</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="sobre" href="">Sobre</a>
+                            <a class="nav-link text-dark" id="sobre" href="">Sobre</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="contato" href="">Loja Física</a>
+                        <li class="nav-iteme">
+                            <a class="nav-link text-dark" id="contato" href="">Loja Física</a>
                         </li>
                     </ul>
                 </div>
